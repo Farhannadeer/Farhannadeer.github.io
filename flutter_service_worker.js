3e26736cf20811ddb630636b9f2c350d363f196e
Farhannadeer <63451420+Farhannadeer@users.noreply.github.com>
2021-02-07 22:27:16 +0530

    ninth

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f4bfce66f9b4913903f7c93c150b62bc",
-"index.html": "2dc001d2b7f9646806f7da27f1676515",
-"/": "2dc001d2b7f9646806f7da27f1676515",
-"main.dart.js": "49330fa500a44e0a19777f47dd960a54",
+"index.html": "d5e68cb1adcea8704c420ec6b8755b02",
+"/": "d5e68cb1adcea8704c420ec6b8755b02",
+"main.dart.js": "a92687899db4a8996e780813975fbd1d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
